e6c801b450eeedd0763065fef9a1176df139f7cc
Zain kiani <zainkiani247@gmail.com>
2023-06-02 09:29:05 +0500

    colors project html & css done

---
+++ app.js
@@ -0,0 +1,79 @@
+// Global selections and variables
+
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll(".color h2");
+let initialColors;
+
+
+
+// Functions
+
+
+// color generator function
+function generateHex(){
+    const hexColor = chroma.random();
+    return hexColor;
+}
+
+function randomColors(){
+    colorDivs.forEach((div,index) => {
+        const hexText = div.children[0];
+        const randomColor = generateHex();
+    
+    //add color to the background
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+    // check contrast
+    checkTextContrast(randomColor,hexText);
+
+    // initial color sliders
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll(".sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+    colorizeSliders(color,hue,saturation,brightness);
+
+});
+}
+
+function checkTextContrast(color,text){
+    const luminance = chroma(color).luminance();
+    if(luminance > 0.5){
+        text.style.color = "black";
+
+    } else {
+        text.style.color = "white";
+    }
+}
+
+function colorizeSliders(color,hue,saturation,brightness){
+    //scale saturation
+    const noSat = color.set("hsl.s",0);
+    const fullSat = color.set("hsl.s",1);
+    const scaleSat = chroma.scale([noSat,color,fullSat]);
+
+    //scale brightness
+
+    const midBright = color.set("hsl.l",0.5);
+    const scaleBright = chroma.scale(["black",midBright,"white"]);
+
+    //scale hue
+
+    // const noHue = color.set("hsl.h",0);
+    // const midHue = color.set("hsl.h",0.5);
+    // const fullHue = color.set("hsl.h",1);
+    // const scaleHue = chroma.scale([noHue,color,fullHue]);
+
+
+    //update input colors
+    saturation.style.backgroundImage = `linear-gradient(to right,${scaleSat(0)}, ${scaleSat(1)})`;
+    brightness.style.backgroundImage = `linear-gradient(to right,${scaleBright(0)},${scaleBright(0.5)}, ${scaleBright(1)})`;
+    hue.style.backgroundImage = `linear-gradient(to right,rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
+}
+
+
+
+randomColors();
